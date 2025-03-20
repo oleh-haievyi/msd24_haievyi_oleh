72c2c1f1591c0ef5d464b46c8e2ac2ef40d3121c
oleh-haievyi <oleh.haievyi@edu.fh-joanneum.at>
2025-03-20 18:35:31 +0100

    JUnit Tests und .gitignore aktualisiert

---
+++ src/test/java/at/fhj/msd/AppTest.java
@@ -0,0 +1,37 @@
+package at.fhj.msd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(5.0, calc.add(2, 3));
+    }
+
+    @Test
+    public void testSubtraction() {
+        Calculator calc = new Calculator();
+        assertEquals(1.0, calc.minus(3, 2));
+    }
+
+    @Test
+    public void testMultiplication() {
+        Calculator calc = new Calculator();
+        assertEquals(6.0, calc.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivision() {
+        Calculator calc = new Calculator();
+        assertEquals(2.0, calc.divide(6, 3));
+    }
+
+    @Test
+    public void testDivisionByZero() {
+        Calculator calc = new Calculator();
+        assertThrows(ArithmeticException.class, () -> calc.divide(1, 0));
+    }
+}  
