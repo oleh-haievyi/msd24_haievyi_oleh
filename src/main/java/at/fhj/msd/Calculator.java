bcd76294c1064f3fe23d0726b0c71395143670e6
oleh-haievyi <oleh.haievyi@edu.fh-joanneum.at>
2025-04-03 11:05:45 +0200

    Factorial metod

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -10,11 +10,21 @@ public class Calculator {
     }
 
     public double divide(double number1, double number2) {
-        if (number2 == 0) throw new ArithmeticException("Division by zero");
+        if (number2 == 0)
+            throw new ArithmeticException("Division by zero");
         return number1 / number2;
     }
 
     public double multiply(double number1, double number2) {
         return number1 * number2;
     }
+
+    public int factorial(int n) { // simple factorial
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
 }
