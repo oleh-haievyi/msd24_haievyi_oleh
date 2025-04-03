5980a3c77386c12813fcc0c42ed7ce24b56ee6ae
oleh-haievyi <oleh.haievyi@edu.fh-joanneum.at>
2025-04-03 13:13:51 +0200

    Clean code and remove comments

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -18,14 +18,6 @@ public class Calculator {
     public double multiply(double number1, double number2) {
         return number1 * number2;
     }
-
-    /*public int factorial(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result = result * i;
-        }
-        return result;
-    }*/
     
     public int factorial(int n) {
         return (n <= 1) ? 1 : n * factorial(n - 1);
