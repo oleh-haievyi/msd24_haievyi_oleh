3de71a24a7a1409fe074cf5d77ef76a73074866d
oleh-haievyi <oleh.haievyi@edu.fh-joanneum.at>
2025-04-28 11:57:59 +0200

    add screenshots to exercise6 documentation

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -3,23 +3,13 @@ package at.fhj.msd;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-<<<<<<< HEAD
-public class Calculator {
-
-    //statik logger for Calculator class
-    private static final Logger logger = LogManager.getLogger(Calculator.class);
-
-    public double add(double number1, double number2) {
-
-        // Log debug mesage for add method
-=======
 /**
  * A simple calculator that can add, subtract, multiply, divide numbers
  * and calculate the factorial of a number.
  */
 public class Calculator {
 
-    //static logger for Calculator class
+    // static logger for Calculator class
     private static final Logger logger = LogManager.getLogger(Calculator.class);
 
     /**
@@ -30,16 +20,10 @@ public class Calculator {
      * @return the sum of number1 and number2
      */
     public double add(double number1, double number2) {
->>>>>>> 90c8d8a (add good style guide page and fix site navigation)
         logger.debug("Calling add with number1=" + number1 + " and number2=" + number2);
         return number1 + number2;
     }
 
-<<<<<<< HEAD
-    public double minus(double number1, double number2) {
-
-        // Log debug mesage for minus method
-=======
     /**
      * Subtracts the second number from the first number.
      *
@@ -48,20 +32,10 @@ public class Calculator {
      * @return the result of number1 minus number2
      */
     public double minus(double number1, double number2) {
->>>>>>> 90c8d8a (add good style guide page and fix site navigation)
         logger.debug("Calling minus with number1=" + number1 + " and number2=" + number2);
         return number1 - number2;
     }
 
-<<<<<<< HEAD
-    public double divide(double number1, double number2) {
-
-        // Log debug mesage for divide method
-        logger.debug("Calling divide with number1=" + number1 + " and number2=" + number2);
-        if (number2 == 0) {
-
-            // Log error message if divide by zero and throw exception
-=======
     /**
      * Divides the first number by the second number.
      *
@@ -73,18 +47,12 @@ public class Calculator {
     public double divide(double number1, double number2) {
         logger.debug("Calling divide with number1=" + number1 + " and number2=" + number2);
         if (number2 == 0) {
->>>>>>> 90c8d8a (add good style guide page and fix site navigation)
             logger.error("Division by zero error, number1=" + number1 + " and number2=" + number2);
             throw new ArithmeticException("Division by zero");
         }
         return number1 / number2;
     }
 
-<<<<<<< HEAD
-    public double multiply(double number1, double number2) {
-
-        // Log debug mesage for multiply method
-=======
     /**
      * Multiplies two numbers together.
      *
@@ -93,16 +61,10 @@ public class Calculator {
      * @return the product of number1 and number2
      */
     public double multiply(double number1, double number2) {
->>>>>>> 90c8d8a (add good style guide page and fix site navigation)
         logger.debug("Calling multiply with number1=" + number1 + " and number2=" + number2);
         return number1 * number2;
     }
 
-<<<<<<< HEAD
-    public int factorial(int n) {
-
-        // Log debug mesage for factorial method
-=======
     /**
      * Calculates the factorial of a number.
      *
@@ -110,7 +72,6 @@ public class Calculator {
      * @return the factorial of n
      */
     public int factorial(int n) {
->>>>>>> 90c8d8a (add good style guide page and fix site navigation)
         logger.debug("Calling factorial with n=" + n);
         return (n <= 1) ? 1 : n * factorial(n - 1);
     }
